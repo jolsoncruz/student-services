289772b13e15884c878e1b01cf7c74611a7eb7f3
aryanxarora <aryanxarora@gmail.com>
2024-02-23 15:15:11 -0500

    Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl' : '1536px'
+    },
+    fontFamily: {   
+      'sans' : ['Inter', 'ui-sans-serif', 'system-ui'],
+      'serif': ['Playfair Display'],
+      'mono' : ['IBM Plex Mono']
+    },
     extend: {},
   },
   plugins: [],
